8de87f9d4533e1a8181a22fbf3e5e045237fcf17
sangjun <jesusfollow1103@naver.com>
2022-05-01 19:34:22 +0900

    study: chapter5(공용 저장 공간을 이용하여 값 뒤로 주고 받기)

--- SubmitValue-Back/SubmitValue-Back/AppDelegate.swift
+++ SubmitValue-Back/SubmitValue-Back/AppDelegate.swift
@@ -10,6 +10,12 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    
+    //값을 저장할 변수 정의
+    var paramEmail : String?
+    var paramUpdate : Bool?
+    var paramInterval :Double?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- SubmitValue-Back/SubmitValue-Back/FormViewController.swift
+++ SubmitValue-Back/SubmitValue-Back/FormViewController.swift
@@ -21,18 +21,32 @@ class FormViewController: UIViewController {
    
     
     @IBAction func onSubmit(_ sender: UIButton) {
-        let preVC = self.presentingViewController
-        guard let vc = preVC as? ViewController else { return }
+//        let preVC = self.presentingViewController
+//        guard let vc = preVC as? ViewController else { return }
         
         //값 전달
-        vc.paramEmail = self.email.text
-        vc.paramUpdate = self.isUpdate.isOn
-        vc.paramInterval = self.interval.value
+//        vc.paramEmail = self.email.text
+//        vc.paramUpdate = self.isUpdate.isOn
+//        vc.paramInterval = self.interval.value
         
         //이전 화면으로 복귀
+//        self.presentingViewController?.dismiss(animated: true)
+        
+        
+        //AppDelegate 객체의 인스턴스를 가져옴
+//        let ad = UIApplication.shared.delegate as? AppDelegate
+//
+//        ad?.paramEmail = self.email.text
+//        ad?.paramUpdate = self.isUpdate.isOn
+//        ad?.paramInterval = self.interval.value
+    
+        let ud = UserDefaults.standard
+        
+        ud.set(self.email.text, forKey: "email")
+        ud.set(self.isUpdate.isOn, forKey: "isUpdate")
+        ud.set(self.interval.value, forKey: "interval")
+        
         self.presentingViewController?.dismiss(animated: true)
-        
-        
     }
     
 }
