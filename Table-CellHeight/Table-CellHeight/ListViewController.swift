a3b204be133f9a9803741d749448ae249f62fb52
sangjun <jesusfollow1103@naver.com>
2022-05-15 17:48:07 +0900

    study: ch8(UITableView sizing)

--- Table-CellHeight/Table-CellHeight/ListViewController.swift
+++ Table-CellHeight/Table-CellHeight/ListViewController.swift
@@ -38,5 +38,39 @@ class ListViewController: UITableViewController {
         
         //알림창 띄우기
         self.present(alert, animated: false)
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.estimatedRowHeight = 50
+        self.tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // "cell" 아이디를 가진 셀을 읽어옴, 없으면 UITableViewCell 인스턴스 생성
+        // A ?? B: A가 nil이 아닐 경우 옵셔널을 해제하고, nil일 경우 대신 B 값을 사용!!
+        // cell 아이디를 가진 셀을 읽어와 옵셔널을 해제하되, 만약 그 값이 nil일 경우 UITableViewCell 인스턴스를 새로 생성!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+        
+        
+        //셀의 기본 텍스트 레이블 행 수 제한을 없앤다.
+        cell.textLabel?.numberOfLines = 0
+        
+        //셀의 기본 텍스트 레이블에 배열 변수의 값을 할당한다.
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        let row = self.list[indexPath.row]
+//        
+//        //높이 조절. 기본 높이 60 + 글의 길이가 30자를 넘어갈 때마다 20씩 높이 증가
+//        let height = CGFloat(60 + (row.count / 30) * 20)
+//        return height
+//    }
 }
