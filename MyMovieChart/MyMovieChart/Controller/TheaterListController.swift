3c20082e68644118c6d8a3b701c48fbae07a1190
sangjun <jesusfollow1103@naver.com>
2022-07-30 23:41:23 +0900

    study: TabBarController, MapKit

---
+++ MyMovieChart/MyMovieChart/Controller/TheaterListController.swift
@@ -0,0 +1,83 @@
+//
+//  TheaterListController.swift
+//  MyMovieChart
+//
+//  Created by 이상준 on 2022/07/30.
+//
+
+import Foundation
+import UIKit
+
+class TheaterListController: UITableViewController {
+    
+    // API를 통해 불러온 데이터를 저장할 배열 변수
+    var list = [NSDictionary]()
+    var startPoint = 0
+    
+    override func viewDidLoad() {
+        self.callTheaterAPI()
+    }
+    
+    func callTheaterAPI() {
+        // URL 구성
+        let requestURI = "http://swiftapi.rubypaper.co.kr:2029/theater/list" // API 요청 주소
+        let sList = 100 // 불러올 데이터 갯수
+        let type = "json" // 데이터 형식
+        
+        let urlObj = URL(string: "\(requestURI)?s_page=\(self.startPoint)&s_list=\(sList)&type=\(type)")
+        
+        do {
+            // NSString 객체를 이용하여 API를 호출하고 그 결과값을 인코딩된 문자열로 받아온다.
+            let stringdata = try NSString(contentsOf: urlObj!, encoding: 0x80_000_422)
+            
+            // 문자열로 받은 데이터를 UTF-8로 인코딩처리한 Data로 변환
+            let encdata = stringdata.data(using: String.Encoding.utf8.rawValue)
+            
+            do {
+                // Data 객체를 파싱하여 NSArray 객체로 변환
+                let apiArray = try JSONSerialization.jsonObject(with: encdata!, options: []) as? NSArray
+                
+                for obj in apiArray! {
+                    self.list.append(obj as! NSDictionary)
+                }
+            } catch {
+                let alert = UIAlertController(title: "실패", message: "데이터 분석이 실패하였습니다.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+                self.present(alert, animated: false)
+            }
+            // 읽어와야 할 다음 페이지의 데이터 시작 위치를 구해 저장해둔다.
+            self.startPoint += sList
+        } catch {
+            let alert = UIAlertController(title: "실패", message: "데이터를 불러오는데 실패하였습니다.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: false)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let obj = self.list[indexPath.row]
+        
+        // 재사용 큐로부터 tCell 식별자에 맞는 셀 객체를 전달받음
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tCell") as! TheaterCell
+        
+        cell.name?.text = obj["상영관명"] as? String
+        cell.tel?.text = obj["연락처"] as? String
+        cell.addr?.text = obj["소재지도로명주소"] as? String
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "segue_map") {
+            let path = self.tableView.indexPath(for: sender as! UITableViewCell)
+            let data = self.list[path!.row]
+            (segue.destination as? TheaterViewController)?.param = data
+            
+        }
+    }
+}
---
+++ MyMovieChart/MyMovieChart/View/ TheaterCell.swift
@@ -0,0 +1,16 @@
+//
+//   TheaterCell.swift
+//  MyMovieChart
+//
+//  Created by 이상준 on 2022/07/30.
+//
+
+import Foundation
+import UIKit
+
+class TheaterCell: UITableViewCell {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var tel: UILabel!
+    @IBOutlet var addr: UILabel!
+}
